8843277f7bfed0966f4c6fafd6abe7b59f76b7cd
Nazi <pikobiznes@gmail.com>
2022-09-11 14:34:01 +0500

    1

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int reverted=0,lastNum;
+        while (number>0)
+        {
+            lastNum=number%10;
+            number/=10;
+            reverted=reverted+lastNum;
+        }
+        System.out.println(reverted);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        int reverted=0,lastNum;
+        while (number>0)
+        {
+            lastNum=number%10;
+            number/=10;
+            reverted=reverted*10+lastNum;
+        }
+        System.out.println(reverted);
     }
 }
